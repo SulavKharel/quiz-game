7e73763deee59e0ae3b752cfb2f720ce11937961
sulav kharel <kharel.sulav2016@gmail.com>
2019-09-15 22:28:50 +0900

    create the questions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  get 'questions/index'
+  get 'questions/show'
+  get 'questions/create'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
